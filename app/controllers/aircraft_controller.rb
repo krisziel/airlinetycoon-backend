cb2a6eb8023d1e811df10dddf473124ebf7b90cd
krisziel <krisziel@mac.com>
2015-03-27 14:08:41 -0700

    aircraft list returns user's a/c of type

--- app/controllers/aircraft_controller.rb
+++ app/controllers/aircraft_controller.rb
@@ -1,24 +1,32 @@
 class AircraftController < ApplicationController
+  before_action :airline
 
   def all
-    aircrafts = Aircraft.all
-    aircraft_list = []
-    aircrafts.each do |aircraft|
-      this_aircraft = {
-        manufacturer:aircraft.manufacturer,
-        name:aircraft.name,
-        full_name:aircraft.full_name,
-        iata:aircraft.iata,
-        capacity:aircraft.capacity,
-        speed:aircraft.speed,
-        turn_time:aircraft.turn_time,
-        price:aircraft.price,
-        discount:aircraft.discount,
-        range:aircraft.range,
-        sqft:aircraft.sqft,
-        user:{inuse:0,unused:0}
-      }
-      aircraft_list.push(this_aircraft)
+    if airline
+      aircrafts = Aircraft.all
+      aircraft_list = []
+      aircrafts.each do |aircraft|
+        user_aircrafts = UserAircraft.where(airline_id:airline.id,aircraft_id:aircraft.id)
+        user_aircraft_list = []
+        user_aircrafts.each{|ac| user_aircraft_list.push(ac.id) }
+        this_aircraft = {
+          manufacturer:aircraft.manufacturer,
+          name:aircraft.name,
+          full_name:aircraft.full_name,
+          iata:aircraft.iata,
+          capacity:aircraft.capacity,
+          speed:aircraft.speed,
+          turn_time:aircraft.turn_time,
+          price:aircraft.price,
+          discount:aircraft.discount,
+          range:aircraft.range,
+          sqft:aircraft.sqft,
+          user:{inuse:0,unused:0,aircraft:user_aircraft_list}
+        }
+        aircraft_list.push(this_aircraft)
+      end
+    else
+      aircraft_list = {error:'user not logged in'}
     end
     render json: aircraft_list
   end
