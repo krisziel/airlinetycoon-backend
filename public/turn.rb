5b11631edb36fc7000ac0a6b0e9f645a032ffa3d
krisziel <krisziel@mac.com>
2015-04-14 19:44:40 -0700

    computes demand in a cabin based on market/price

--- public/turn.rb
+++ public/turn.rb
@@ -33,4 +33,65 @@ class TurnController < ApplicationController
     end
   end
 
+  def price_spread market, fare
+    elasticity = {
+      [1.25,10] => {
+        base:0.4509,
+        margin:1.009,
+        anchor:1.25
+      },
+      [1.01,1.24] => {
+        base:0.0,
+        margin:1.015,
+        anchor:1.0
+      },
+      [1,1] => {
+        base:0.0,
+        margin:1,
+        anchor:1.0
+      },
+      [0.9,1] => {
+        base:0.0,
+        margin:0.994,
+        anchor:1.0
+      },
+      [0.7,0.89] => {
+        base:-0.0584,
+        margin:0.975,
+        anchor:0.9
+      },
+      [0,0.69] => {
+        base:-0.4325,
+        margin:0.95,
+        anchor:0.7
+      }
+    }
+    demand = {
+      :spread => 1.0,
+      :elasticity => {},
+      :percent => 0,
+      :multiplier => 1
+    }
+    market = market.to_f
+    fare = fare.to_f
+    spread = (market-fare)/market
+    demand[:spread] = spread.round(4)
+    demand[:percent] = (demand[:spread].abs*100).round
+    elasticity.each do |key,value|
+      if (1.0+demand[:spread]).between?(key[0],key[1])
+        demand[:elasticity] = value
+      end
+    end
+    demand[:multiplier] = compute_multiplier demand
+    demand
+  end
+
+  def compute_multiplier demand
+    exponent = ((1.0+demand[:spread])-demand[:elasticity][:anchor])
+    exponent = (exponent*100).abs.floor.to_i
+    multiplier = (demand[:elasticity][:margin]**exponent)+demand[:elasticity][:base]
+    multiplier = multiplier.round(4)
+    multiplier
+  end
+
 end
