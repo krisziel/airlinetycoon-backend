0e824c427901404c529a99b2348c866fad28c233
krisziel <krisziel@mac.com>
2015-04-15 17:03:04 -0700

    starting to sort flights by class fare

--- public/turn.rb
+++ public/turn.rb
@@ -72,8 +72,8 @@ class Turn
       route[:flights] = flights
       reformatted_routes.push(route)
     end
-    p reformatted_routes
     reformatted_routes
+    @compare = reformatted_routes[3]
   end
 
   def process_route route
@@ -174,6 +174,49 @@ class Turn
     multiplier = multiplier.round(4)
   end
 
+  def sort_fares route
+    sorted_fares = {
+      f:[],
+      j:[],
+      p:[],
+      y:[]
+    }
+    route[:flights].each do |flight|
+      reformat = reform_sort_flight flight
+      sorted_fares[:f].push(reformat[:f])
+      sorted_fares[:j].push(reformat[:j])
+      sorted_fares[:p].push(reformat[:p])
+      sorted_fares[:y].push(reformat[:y])
+    end
+    sorted_fares[:f].sort_by {|_key, value| value[:fare]}
+    sorted_fares[:j].sort_by {|_key, value| value[:fare]}
+    sorted_fares[:p].sort_by {|_key, value| value[:fare]}
+    sorted_fares[:y].sort_by {|_key, value| value[:fare]}
+    sorted_fares
+  end
+
+  def reform_sort_flight flight
+    reformat = {
+      f:{},
+      j:{},
+      p:{},
+      y:{}
+    }
+    flight[:cabins].each do |key,cabin|
+      reformat[key] = {
+        :id => flight[:id],
+        :fare => cabin[:fare],
+        :pricing => cabin[:pricing],
+        :count => cabin[:count]
+      }
+    end
+    reformat
+  end
+  # {:id=>48, :cabins=>{:f=>{:fare=>4535, :count=>10, :pricing=>{:spread=>0.2709, :elasticity=>{:base=>0.4509, :margin=>1.009, :anchor=>1.25},
+  # :percent=>27, :multiplier=>1.469}}, :j=>{:fare=>1948, :count=>58, :pricing=>{:spread=>0.5824, :elasticity=>{:base=>0.4509, :margin=>1.009,
+  #   :anchor=>1.25}, :percent=>58, :multiplier=>1.7949}}, :p=>{:fare=>605, :count=>63, :pricing=>{:spread=>0.7359, :elasticity=>{:base=>0.4509,
+  #     :margin=>1.009, :anchor=>1.25}, :percent=>74, :multiplier=>1.9883}}, :y=>{:fare=>672, :count=>232, :pricing=>{:spread=>0.3984,
+  #       :elasticity=>{:base=>0.4509, :margin=>1.009, :anchor=>1.25}, :percent=>40, :multiplier=>1.5845}}}}
   def compare_demand route
 
   end
