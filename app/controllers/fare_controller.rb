b91c568eb7e3d58a43937cee7ccfccae972dfb26
Kris <krisziel@mac.com>
2015-10-22 03:18:05 -0700

    mild refactoring

--- app/controllers/fare_controller.rb
+++ app/controllers/fare_controller.rb
@@ -19,44 +19,52 @@ class FareController < ApplicationController
 
   def get_routings origin, destination
     airline = Airline.find(3)
-    origin_destinations = get_destinations origin
-    destination_destinations = get_destinations destination
-    routes = origin_destinations[:destinations] & destination_destinations[:destinations]
-    origin_destinations[:destinations].each do |airport|
-      flight = airline.flights.find_by(route_id:Route.where("(origin_id=? AND destination_id IN (?)) OR (origin_id IN (?) AND destination_id=?)", airport, destination_destinations[:destinations], destination_destinations[:destinations], airport))
+    nonstop = airline.flights.find_by(route_id:Route.where("(origin_id=? AND destination_id=?) OR (origin_id=? AND destination_id=?)", origin, destination, destination, origin))
+    route_id = nonstop ? nonstop.route_id : 0
+    origin_pairs = get_destinations origin, route_id
+    destination_pairs = get_destinations destination, route_id
+    routes = origin_pairs[:airports] & destination_pairs[:airports]
+    route_list = []
+    origin_pairs[:airports].each_with_index do |airport, index|
+      if origin_pairs[:routes][index] == route_id
+        origin_pairs[:routes].delete(route_id)
+        origin_pairs[:airports].delete(airport)
+        next
+      end
+      flight = airline.flights.where("route_id != ?", (nonstop ? nonstop.route_id : 0)).find_by(route_id:Route.where("(origin_id=? AND destination_id IN (?)) OR (origin_id IN (?) AND destination_id=?)", airport, destination_pairs[:airports], destination_pairs[:airports], airport))
       if flight
         origin = flight.route.origin_id
-        outbound = origin_destinations[:destinations].index(airport)
-        inbound = destination_destinations[:destinations].index(origin == airport ? flight.route.destination_id : origin)
-        pair = [origin_destinations[:routes][outbound], flight.route_id, destination_destinations[:routes][inbound]]
-        routes.push(pair)
+        outbound = origin_pairs[:airports].index(airport)
+        inbound = destination_pairs[:airports].index(origin == airport ? flight.route.destination_id : origin)
+        pair = [origin_pairs[:routes][outbound], flight.route_id, destination_pairs[:routes][inbound]]
+        route_list.push(pair)
+        routes.delete(airport)
       end
     end
-    route_list = []
-    routes.each_with_index do |route, index|
-      if route.class == Array
-        route_list.push(route)
-      else
-        outbound = origin_destinations[:destinations].index(route)
-        inbound = destination_destinations[:destinations].index(route)
-        pair = [origin_destinations[:routes][outbound], destination_destinations[:routes][inbound]]
-        route_list.push(pair)
-      end
+    routes.each do |route|
+      outbound = origin_pairs[:airports].index(route)
+      inbound = destination_pairs[:airports].index(route)
+      pair = [origin_pairs[:routes][outbound], destination_pairs[:routes][inbound]]
+      route_list.unshift(pair)
+    end
+    if nonstop
+      route_list.unshift([nonstop.route_id])
     end
     return route_list.uniq
   end
 
-  def get_destinations airport
+  def get_destinations airport, route_id
     airline = Airline.find(3)
-    flights = airline.flights.where(route_id:Route.where("origin_id = ? OR destination_id = ?", airport, airport))
-    destination_list = []
+    flights = airline.flights.where(route_id:Route.where("origin_id = ? OR destination_id = ? AND id != ?", airport, airport, route_id))
+    airport_list = []
     route_list = []
     flights.each do |flight|
       dest = flight.route.destination_id
-      destination_list.push(dest == airport ? flight.route.origin_id : dest)
+      airport_list.push(dest == airport ? flight.route.origin_id : dest)
       route_list.push(flight.route.id)
     end
-    return { destinations:destination_list, routes:route_list }
+    return { airports:airport_list, routes:route_list }
   end
 
 end
+# Flight.last.update(airline_id:3)
