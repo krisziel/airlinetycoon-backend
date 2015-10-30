946e15402acd7b1cdf6eef445c0cf78f2e57e9c4
Kris <krisziel@mac.com>
2015-10-30 00:22:10 -0700

    comments for the complicated fare stuff

--- app/controllers/fare_controller.rb
+++ app/controllers/fare_controller.rb
@@ -3,8 +3,8 @@ class FareController < ApplicationController
 
   def show
     if airline
-      routings_data = get_routings params[:o], params[:d]
-      route_data = extract_routes routings_data
+      routings_data = get_routings params[:o], params[:d] # get every possible routing between the two airports
+      route_data = extract_routes routings_data # just reformats the data from each route for easier display client side
       data = {
         airports:route_data[:airports],
         routes:route_data[:routes],
@@ -17,19 +17,21 @@ class FareController < ApplicationController
   def create
     if airline
       modified_routings = []
-      price = JSON.parse(params[:fare])
-      routings = JSON.parse(params[:routings])
+      price = JSON.parse(params[:fare]) # price is really the fare
+      routings = JSON.parse(params[:routings]) # routings is array of arrays of all route ids selected
       route_id = params[:route_id]
-      fare = airline.fares.new(route_id:route_id, fare:price)
+      fare = airline.fares.new(route_id:route_id, fare:price) # create new fare with the fare and route id
       if fare.save
-        capacity = fare_capacity routings
+        capacity = fare_capacity routings # grab the total capacity for all selected routings
         routings.each do |routing|
-          new_routing = fare.fare_routings.new(routing:routing)
+          new_routing = fare.fare_routings.new(routing:routing) # create a new fare routing for each routing within fare
           if new_routing.save
-            modified_routings.push(new_routing.id)
+            modified_routings.push(new_routing.id) # push the new fare routing into array for the fare
           end
         end
-        fare.update(routings:modified_routings, capacity:capcaity)
+        fare.update(routings:modified_routings, capacity:capacity)
+      elsif fare.errors
+        fare = airline.fares.find_by(route_id:route_id)
       end
       render json: fare
     else
@@ -43,7 +45,6 @@ class FareController < ApplicationController
     routings = get_routings route.origin_id, route.destination_id
     routings.each do |route|
       airports = route
-
     end
   end
 
@@ -83,12 +84,11 @@ class FareController < ApplicationController
   end
 
   def route_capacity route
-    airline = Airline.find(3)
-    capacity = { :f=>0, :j=>0, :p=>0, :y=>0, :route=>route }
+    capacity = { :f=>0, :j=>0, :p=>0, :y=>0, :route=>route } # will hold total seats on route + route id
     flights = airline.flights.where(route_id:route)
     flights.each do |flight|
-      flight_capacity = flight.user_aircraft.aircraft_configuration
-      capacity[:f] += (flight.frequencies * flight_capacity.f_count)
+      flight_capacity = flight.user_aircraft.aircraft_configuration # get the aircraft configuration
+      capacity[:f] += (flight.frequencies * flight_capacity.f_count) # and add seats from each cabin to existing seat count
       capacity[:j] += (flight.frequencies * flight_capacity.j_count)
       capacity[:p] += (flight.frequencies * flight_capacity.p_count)
       capacity[:y] += (flight.frequencies * flight_capacity.y_count)
@@ -99,12 +99,12 @@ class FareController < ApplicationController
   def extract_routes routings
     routes = {}
     airports = {}
-    routings.each do |routing|
-      routing.each do |route_id|
+    routings.each do |routing| # for each routing array within array of all routings
+      routing.each do |route_id| # go through each route within that individual route
         route = Route.find(route_id)
-        routes[route_id] = route_info route
-        airports[route.origin_id] = route.origin.basic_data
-        airports[route.destination_id] = route.destination.basic_data
+        routes[route_id] = route_info route # get route id, airports, capacity
+        airports[route.origin_id] = route.origin.basic_data # put the airport basic info in an array to be returned
+        airports[route.destination_id] = route.destination.basic_data # for testing as client side already has full airport list
       end
     end
     {
@@ -114,25 +114,19 @@ class FareController < ApplicationController
   end
 
   def route_info route
-    airline = Airline.find(3)
     flights = route.flights.where(airline_id: airline)
     route_info = {
       capacity:get_route_capacity(flights),
       origin:route.origin_id,
       destination:route.destination_id
     }
-    route_info
+    route_info # just returning reformatted data and getting total route capacity
   end
 
   def get_route_capacity flights
-    capacity = {
-      :y => 0,
-      :p => 0,
-      :j => 0,
-      :f => 0
-    }
+    capacity = { :y => 0, :p => 0, :j => 0, :f => 0 }
     flights.each do |flight|
-      capacity = add_capacity(capacity, flight.frequencies, flight.user_aircraft.aircraft_configuration)
+      capacity = add_capacity(capacity, flight.frequencies, flight.user_aircraft.aircraft_configuration) # add capacity from each flight to existing capacity
     end
     capacity
   end
@@ -142,43 +136,44 @@ class FareController < ApplicationController
     existing[:p] += flight.p_count*frequencies
     existing[:j] += flight.j_count*frequencies
     existing[:f] += flight.f_count*frequencies
-    existing
+    existing # add seats from flight to existing capacity and return full capacity
   end
 
   def get_routings origin, destination
-    nonstop = airline.flights.find_by(route_id:Route.where("(origin_id=? AND destination_id=?) OR (origin_id=? AND destination_id=?)", origin, destination, destination, origin))
-    route_id = nonstop ? nonstop.route_id : 0
-    origin_pairs = get_destinations origin, route_id
-    destination_pairs = get_destinations destination, route_id
+    nonstop = airline.flights.find_by(route_id:Route.where("(origin_id=? AND destination_id=?) OR (origin_id=? AND destination_id=?)", origin, destination, destination, origin)) # get nonstop if there is one
+    route_id = nonstop ? nonstop.route_id : 0 # if there isn't a nonstop, set 0 since it is not a route id
+    origin_pairs = get_destinations origin, route_id # get all destinations from the route's origin
+    destination_pairs = get_destinations destination, route_id # get all destinations from the route's destination
     route_list = []
-    origin_pairs.each do |airport, route|
-      if destination_pairs[airport]
-        route_list.push([route, destination_pairs[airport]])
+    origin_pairs.each do |airport, route| # for every destination from the origin
+      if destination_pairs[airport] # check if is is also a destination for the route destination
+        route_list.push([route, destination_pairs[airport]]) # put the array of the two route ids in the route list array
       end
       flights = airline.flights.where("route_id != ?", (nonstop ? nonstop.route_id : 0)).where(route_id:Route.where("(origin_id=? AND destination_id IN (?)) OR (origin_id IN (?) AND destination_id=?)", airport, destination_pairs.keys, destination_pairs.keys, airport))
-      flights.each do |flight|
+      # get flights that aren't the non-stop that are between this airport and every airport served by the route's destination
+      flights.each do |flight| # for each flight matching that
         origin = flight.route.origin_id
         dest = flight.route.destination_id
-        connection = (origin == airport ? dest : origin)
-        route_list.push([route, flight.route.id, destination_pairs[connection]])
+        connection = (origin == airport ? dest : origin) # the connection is between this airport and the airport from the destination list that matches it
+        route_list.push([route, flight.route.id, destination_pairs[connection]]) # put the array of all three route ids in the route list array
       end
     end
     if nonstop
-      route_list.unshift([nonstop.route_id])
+      route_list.unshift([nonstop.route_id]) # if there is a nonstop, put it in the front of the array
     end
     return route_list.uniq
   end
 
   def get_destinations airport, route_id
-    flights = airline.flights.where(route_id:Route.where("(origin_id = ? OR destination_id = ?) AND id != ?", airport, airport, route_id))
+    flights = airline.flights.where(route_id:Route.where("(origin_id = ? OR destination_id = ?) AND id != ?", airport, airport, route_id)) # get all flights from this airport except non-stop to end point
     airport_list = []
     route_list = []
     return_data = {}
-    flights.each do |flight|
+    flights.each do |flight| # for every flight the airport has
       dest = flight.route.destination_id
       origin = flight.route.origin_id
-      origin == airport.to_i ? airport_id = dest : airport_id = origin
-      return_data[airport_id] = flight.route.id
+      origin == airport.to_i ? airport_id = dest : airport_id = origin # set the airport_id as the other airport
+      return_data[airport_id] = flight.route.id # and then use the airport as the key and the route as the value so no dupe airports/routes in array
     end
     return return_data
   end
--- lib/fare_parser.rb
+++ lib/fare_parser.rb
@@ -4,55 +4,57 @@ class FareParser
     fare = Fare.find(id)
     airline = fare.airline
     route = fare.route
-    routes_data = []
-    routings = fare.routings
-    routings.each do |routing|
-      duration = (route.distance/smallestPlane(route.distance).speed.to_f)*60
-      routes_data.push({
-        routing: routing,
-        price: get_routing_price(airline, route.price, duration, routing),
-        capacity: get_routing_capacity(airline, route.id, routing)
+    routes_data = [] # will hold a list of all routings for the fare
+    routings = fare.fare_routings # all routings that are part of the fare
+    routings.each do |routing| # for each routing the fare has
+      this_routing = routing.routing # routing is fare routing .routing is array of route ids
+      duration = (route.distance/smallestPlane(route.distance).speed.to_f)*60 # get the duration of the flight for the smallest mission capable plane
+      routes_data.push({ # drop the info for each routing in the array
+        routing: this_routing, # just the array of route ids
+        price: get_routing_price(airline, route.price, duration, this_routing), # get market pricing for the specific routing
+        capacity: get_routing_capacity(airline, route.id, this_routing) # get maximum capacity for the routing
       })
     end
     routes_data
   end
 
-  def get_routing_price airline, price, nonstop_duration, routing
+  def get_routing_price airline, price, nonstop_duration, routing # get pricing for a routing that will provide full market demand
+    # get the duration for every route within the routing and get the shortest flight for each route
     segment_flights = airline.flights.where('route_id IN (?)', routing).select(:duration, :route_id).order(:duration).distinct
-    total_duration = segment_flights.inject(0) { |sum, flight| sum + flight.duration }
+    total_duration = segment_flights.inject(0) { |sum, flight| sum + flight.duration } # total duration of all the flights
+    # discount the non-stop market fare to account for mileage and layovers
+    # get the total duration of the routings, which all flights + 60 minutes for each connection, divide by nonstop to get percentage above market fare
+    # take the sqare route of that to discount a bit less than the increase in time taken and then subtract 1 to get the actual discount rate
     discount_rate = (Math.sqrt((total_duration.to_f+((routing.length-1)*60))/nonstop_duration)-1)
     prices = {}
-    price.each_with_index do |(key,value),index|
+    price.each_with_index do |(key,value),index| # for each cabin, discount a bit more based on number cabin
+      # add'l reduction = 0% for Y, 5% for P, 10% for J, 15% for F to reflect the increasing value of time
       prices[key] = (price[key]*(1-(discount_rate + (index*0.05)))).round
     end
     prices
   end
 
   def get_routing_capacity airline, route_id, routing
+    # get all flights on a route user_aircraft to get capacity and frequencies to get total weekly capacity
     segment_flights = airline.flights.where('route_id IN (?)', routing).select(:user_aircraft_id, :route_id, :frequencies).order(:route_id)
-    capacity = {
-      :y => 99999,
-      :p => 99999,
-      :j => 99999,
-      :f => 99999
-    }
+    capacity = { :y => 99999, :p => 99999,:j => 99999, :f => 99999 } # since we'll call
     capacities = {}
     segment_flights.each do |flight|
       route = flight.route_id
-      if(!capacities[route])
-        capacities[route] = {:y=>0, :p=>0, :j=>0, :f=>0}
+      if(!capacities[route]) # if there is not already capacity for this route
+        capacities[route] = {:y=>0, :p=>0, :j=>0, :f=>0} # set it to zero
       end
-      capacities[route] = add_capacity(capacities[route], flight.frequencies, flight.user_aircraft.aircraft_configuration)
+      capacities[route] = add_capacity(capacities[route], flight.frequencies, flight.user_aircraft.aircraft_configuration) # set the capacity to existing + this flight
     end
-    capacities.each do |route, segment_capacity|
-      segment_capacity.each do |cabin, seats|
-        capacity[cabin] = [capacity[cabin], seats].min
+    capacities.each do |route, segment_capacity| # for each route in capacities
+      segment_capacity.each do |cabin, seats| # get for each cabin
+        capacity[cabin] = [capacity[cabin], seats].min # get the minimum between existing minimum and this flight so capacity is limited to the smallest capacity route
       end
     end
     capacity
   end
 
-  def add_capacity existing, frequencies, flight
+  def add_capacity existing, frequencies, flight # for adding capacity from each subsequent flight on a route
     existing[:y] += flight.y_count*frequencies
     existing[:p] += flight.p_count*frequencies
     existing[:j] += flight.j_count*frequencies
@@ -60,9 +62,9 @@ class FareParser
     existing
   end
 
-  def smallestPlane distance
-    mission_capable = Aircraft.where('range >= ?', distance).order(:capacity).limit(1)
-    if mission_capable.length == 0
+  def smallestPlane distance # returns smallest plane capable of flying a path to compare flight time against multi flight routings
+    mission_capable = Aircraft.where('range >= ?', distance).order(:capacity).limit(1) # get the smallest plane that can fly the route
+    if mission_capable.length == 0 # if there is a not a suitable aircraft, get the aircraft with the longest range
       mission_capable = Aircraft.order(range: :desc).limit(1)
     end
     mission_capable.first
