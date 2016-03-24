e90eaae044b97a3410099e4bbf84c21d43bd68c6
krisziel <krisziel@mac.com>
2016-03-24 01:00:25 -0700

    receive message and send to airline/alliance/game

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -2,7 +2,7 @@ class ChatController < ApplicationController
   before_action :game, :airline
 
   @clients = []
-  @conversations = []
+  @conversations = {}
 
   EM.run do
     EM::WebSocket.start(host: ENV['WEBSOCKET_HOST'], port: ENV['WEBSOCKET_PORT']) do |ws|
@@ -57,58 +57,61 @@ class ChatController < ApplicationController
             user_id:(crypt.decrypt_and_verify(user_id) rescue nil),
             game_id:(crypt.decrypt_and_verify(game_id) rescue nil)
           }
+          puts user_data
           if ((user_data[:user_id] == nil)||(user_data[:game_id] == nil))
             response = {
               error:"bad auth"
             }
           else
+            msg_status = {status:'Error sending message'}
             airline = Airline.find_by(user_id:user_data[:user_id],game_id:user_data[:game_id])
-            type_id = message_data["type_id"]
             type = message_data["message_type"]
-            body = message_data["body"]
-            message = Message.new(body:body, airline_id:airline.id, message_type:type, type_id:type_id)
-            if message.save
-              if message.message_type == "alliance"
-                alliance_airlines = @clients.select {|client| client[:alliance] == message.type_id }
-                if alliance_airlines
-                  alliance_airlines.each do |alliance_airline|
-                    alliance_airline[:socket].send message.serialize
-                  end
-                end
-              elsif message.message_type == "game"
-                game_airlines = @clients.select {|client| client[:game] == message.type_id }
-                if game_airlines
-                  game_airlines.each do |game_airline|
-                    if game_airline[:id] != message.airline_id
-                      game_airline[:socket].send message.serialize.to_json
+            type_id = nil
+            if type == "Alliance"
+              type_id = (airline.alliance ? airline.alliance.id : nil)
+            elsif type == "Airline"
+              # type_id = get_conversation_id airline.id, type_id
+              airline = (Game.find(user_data[:game_id]).airlines.find(message_data["type_id"]).id rescue nil)
+            elsif type == "Game"
+              type_id = user_data[:game_id]
+            end
+            if type_id != nil
+              body = message_data["body"]
+              message = Message.new(body:body, airline_id:airline.id, message_type:type, type_id:type_id)
+              if message.save
+                if message.message_type == "Alliance"
+                  alliance_airlines = @clients.select {|client| client[:alliance] == message.type_id }
+                  if alliance_airlines.length > 0
+                    alliance_airlines.each do |alliance_airline|
+                      alliance_airline[:socket].send message.serialize
                     end
                   end
+                elsif message.message_type == "Game"
+                  game_airlines = @clients.select {|client| client[:game] == message.type_id }
+                  if game_airlines.length > 0
+                    game_airlines.each do |game_airline|
+                      if game_airline[:id] != message.airline_id
+                        game_airline[:socket].send message.serialize.to_json
+                      end
+                    end
+                  end
+                elsif message.message_type == "Airline"
+                  recipient_airline = @clients.select {|client| client[:id] == type_id }
+                  if recipient_airline.length > 0
+                    recipient_airline[0][:socket].send message.serialize.to_json
+                  end
                 end
-              elsif message.message_type == "conversation"
+                msg_status = {status:'Message sent'}
               end
+            else
+              msg_status = {status:'Message recipient not found'}
             end
           end
         end
       end
-
-      def send_message message
-        if message.message_type == "alliance"
-          alliance_airlines = @clients.find {|client| client[:alliance] == message.type_id }
-          if alliance_airlines
-            alliance_airlines.each do |alliance_airline|
-              alliance_airline[:socket].send message.serialize
-            end
-          end
-        elsif message.message_type == "game"
-          game_airlines = @clients.find {|client| client[:game] == message.type_id }
-          if game_airlines
-            game_airlines.each do |game_airline|
-              game_airline[:socket].send message.serialize
-            end
-          end
-        elsif message.message_type == "conversation"
-        end
-      end
+      # ws://localhost:3001?user_cookie=NThWZExFQlVoUzVubTNaVXN5SllQUT09LS0weTlCbEhYZ0tXMHkzQTFQR0l2cU1BPT0=--40387434903cf41f0385c5a1756ec1029f7da12c&game_cookie=L1N4aE9wZnlxT1puL0RWS3RpMWRDUT09LS1HdjdiMzVnd2E4WjQ1Q1NPT21vbVR3PT0=--d86d8807568aae3e57698e02b42d30b720bb754c
+      # ws://localhost:3001?user_cookie=Y2ZKNE9TS3dERGE3RERtMlAwSE11Zz09LS1YVk4wSzY2M3BWd1hFSDJja3o4TkRRPT0=--575eb75ed9fb751c9ce6afbf21e91fae4b0778fa&game_cookie=L1N4aE9wZnlxT1puL0RWS3RpMWRDUT09LS1HdjdiMzVnd2E4WjQ1Q1NPT21vbVR3PT0=--d86d8807568aae3e57698e02b42d30b720bb754c
+      return msg_status.to_json
     end
 
     timer = EventMachine::PeriodicTimer.new(5) do
@@ -124,8 +127,78 @@ class ChatController < ApplicationController
     end
   end
 
+  def send_message message_data, user_data
+  end
+
   def join
     render json: { server: 'started' }
   end
+
+  def get_conversation_id sender, recipient
+    existing_conversation = @conversations.select {|id, conversation| (((conversation[:recipient] == sender)&&(conversation[:sender] == recipient))||((conversation[:sender] == sender)&&(conversation[:recipient] == recipient))) }
+    if existing_conversation.length > 0
+      return existing_conversation.first[0]
+    else
+      conversation = Conversation.find_by("(conversations.sender_id = ? AND conversations.recipient_id = ?) OR (conversations.sender_id = ? AND conversations.recipient_id = ?)", sender, recipient, recipient, sender)
+      if conversation
+        return conversation.id
+      else
+        sender_airline = (Airline.find(sender) rescue nil)
+        recipient_airline = (Airline.find(recipient) rescue nil)
+        if ((sender_airline)&&(recipient_airline)&&(sender_airline.game == recipient_airline.game))
+          new_conversation = Conversation.new(sender_id:sender_airline.id, recipient_id:recipient_airline.id)
+          if new_conversation.save
+            return new_conversation.id
+          else
+            return nil
+          end
+        else
+          return nil
+        end
+      end
+    end
+  end
+
+  # def test
+  #   airlines = [27, 36, 37, 28, 30, 33, 40, 31, 38, 35, 39, 26, 32, 34, 29]
+  #   start = Time.now
+  #   10000.times do |i|
+  #     sender = airlines.sample
+  #     recipient = airlines.sample
+  #     conversation = get_conversation_id sender, recipient
+  #   end
+  #   finish = Time.now
+  #   puts "TOOK #{(finish - start)} SECONDS"
+  # end
+  #
+  # def test2
+  #   airlines = [27, 36, 37, 28, 30, 33, 40, 31, 38, 35, 39, 26, 32, 34, 29]
+  #   start = Time.now
+  #   500.times do |i|
+  #     @conversations[i] = {
+  #       sender:airlines.sample,
+  #       recipient:airlines.sample
+  #     }
+  #   end
+  #   10000.times do |i|
+  #     s = airlines.sample
+  #     r = airlines.sample
+  #     convo = @conversations.select {|id, conversation| (((conversation[:recipient] == s)&&(conversation[:sender] == r))||((conversation[:sender] == s)&&(conversation[:recipient] == r))) }
+  #     @conversations.each do |id, conversation|
+  #       puts conversation[:recipient]
+  #       puts "#{conversation[:recipient]} - #{s} // #{conversation[:sender]} - #{r}"
+  #       a = (conversation[:recipient] == s)
+  #       b = (conversation[:recipient] == r)
+  #       c = (conversation[:sender] == s)
+  #       d = (conversation[:sender] == r)
+  #       puts "#{a} - #{b} - #{c} - #{d}"
+  #       if ((a&&d)||(b&&c))
+  #         break
+  #       end
+  #     end
+  #   end
+  #   finish = Time.now
+  #   puts "TOOK #{(finish - start)} SECONDS"
+  # end
 
 end
--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -3,8 +3,8 @@ class Conversation < ActiveRecord::Base
   belongs_to :recipient, :foreign_key => :recipient_id, class_name: 'Airline'
   has_many :messages, dependent: :destroy
   validates_uniqueness_of :sender_id, :scope => :recipient_id
-  scope :between, -> (sender_id,recipient_id) do
-    where("(conversations.sender_id = ? AND conversations.recipient_id =?) OR (conversations.sender_id = ? AND conversations.recipient_id =?)", sender_id,recipient_id, recipient_id, sender_id)
+  scope :between, -> (sender_id, recipient_id) do
+    where("(conversations.sender_id = ? AND conversations.recipient_id =?) OR (conversations.sender_id = ? AND conversations.recipient_id =?)", sender_id, recipient_id, recipient_id, sender_id)
   end
 
   def last_message
